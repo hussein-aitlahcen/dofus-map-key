cc686a5a5a2331414fb71454911f27fb9672d350
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2017-10-15 14:43:39 +0200

    Improved frequency, implemented alternative metric

--- Program.cs
+++ Program.cs
@@ -25,9 +25,9 @@ namespace DofusKeyFinder
         public int Id;
         public int Width;
         public int Height;
-        public String Key;
-        public String DecodedData;
-        public String EncodedData;
+        public string Key;
+        public string DecodedData;
+        public string EncodedData;
     }
 
     public class Frequencies
@@ -36,28 +36,112 @@ namespace DofusKeyFinder
         public Dictionary<char, float>[] Position;
     }
 
+    public class ComputedKey
+    {
+        public string Value;
+        public Dictionary<int, List<(int, double)>> Alternatives;
+    }
+
 
     /*
         Achieved 75% success xd
      */
     class Program
     {
+        const int ValidKeyAltThreshold = 15;
+        const double ErrorClamp = 1.15;
+        const int CellPatternSize = 10;
         const int MinKeyLength = 128;
         const int MaxKeyLength = 256;
         const int MinKeyXor = 32;
         const int MaxKeyXor = 127;
-        public static String HEX_CHARS = "0123456789ABCDEF";
-        public static String FrequenciesPath = "./frequencies.json";
+        public static string HEX_CHARS = "0123456789ABCDEF";
+        public static string MapsPath = "../maps";
+        public static string FrequenciesPath = "./frequencies.json";
         public static string InputPath = "./input";
         public static string OutputPath = "./output";
 
-        public static Frequencies LoadFrequency()
+        public static IEnumerable<Map> LoadMaps()
+        {
+            return Directory.GetFiles(MapsPath, "*.json")
+                .Select(file => JsonConvert.DeserializeAnonymousType<Map>(File.ReadAllText(file), new Map()));
+        }
+
+        public static Frequencies LoadFrequencies()
         {
             return JsonConvert.DeserializeAnonymousType<Frequencies>(File.ReadAllText(FrequenciesPath), new Frequencies());
         }
+
         public static void Main(String[] args)
         {
-            var frequency = LoadFrequency();
+            var frequencies = LoadFrequencies();
+            //Benchmark(frequencies);
+            ExportCompute(frequencies);
+        }
+
+        public static void Benchmark(Frequencies frequencies)
+        {
+            var hit = 0;
+            var tih = 0;
+            var alt = 0;
+            var bad = 0;
+            foreach (var map in LoadMaps())
+            {
+                tih++;
+                var realKey = PrepareKey(map.Key);
+                var encodedData = HexToString(map.EncodedData);
+                var keyLength = ComputeKeyLengthHamming(encodedData);
+                var key = GuessKey(encodedData, keyLength, frequencies);
+                if (key.Alternatives.Count > ValidKeyAltThreshold)
+                {
+                }
+                if (key.Value == realKey)
+                {
+                    if (key.Alternatives.Count > 0)
+                    {
+                        Console.WriteLine($"ALT: {map.Id}, {key.Alternatives.Count}");
+                    }
+                    hit++;
+                }
+                else
+                {
+                    if (key.Alternatives.Count > 0)
+                    {
+                        bad++;
+                        alt += key.Alternatives.Count;
+                        Console.WriteLine(alt / (float)bad);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"IMP: {map.Id}, {key.Alternatives.Count}");
+                    }
+                }
+            }
+        }
+
+        public static List<string> ComputeAlternatives(ComputedKey key)
+        {
+            var alternatives = new List<string>();
+            var maxOffset = key.Alternatives.Max(kv => kv.Value.Count);
+            for (var i = 1; i < maxOffset; i++)
+            {
+                var altKey = key.Value.ToArray();
+                foreach (var kv in key.Alternatives)
+                {
+                    var positionAlt = kv.Value;
+                    var altOffset = positionAlt.Count - 1 - i;
+                    if (altOffset < 0)
+                        altOffset = positionAlt.Count - 1;
+                    var (c, score) = positionAlt[altOffset];
+                    altKey[kv.Key] = (char)c;
+                }
+                alternatives.Add(new string(altKey));
+            }
+            return alternatives;
+        }
+
+        public static void ExportCompute(Frequencies frequencies)
+        {
             Directory.CreateDirectory(InputPath);
             Directory.CreateDirectory(OutputPath);
             Directory.GetFiles(InputPath, "*")
@@ -65,8 +149,9 @@ namespace DofusKeyFinder
                 .Select(path => new { EncodedData = HexToString(File.ReadAllText(path)), Name = Path.GetFileName(path) })
                 .Do(x => Console.WriteLine("processing: " + x.Name))
                 .Select(x => new { Header = x, KeyLength = ComputeKeyLengthHamming(x.EncodedData) })
-                .Select(y => new { Data = y, Key = FormatKeyExport(GuessKey(y.Header.EncodedData, y.KeyLength, frequency)) })
-                .Do(z => File.WriteAllText(string.Format("{0}/{1}", OutputPath, z.Data.Header.Name + "_key.txt"), z.Key))
+                .Select(y => new { Data = y, Key = GuessKey(y.Header.EncodedData, y.KeyLength, frequencies) })
+                .Do(z => Console.WriteLine($"alternatives: {z.Key.Alternatives.Count}"))
+                .Do(z => File.WriteAllText(string.Format("{0}/{1}", OutputPath, z.Data.Header.Name + "_key.txt"), FormatKeyExport(z.Key.Value)))
                 .ObserveOn(TaskPoolScheduler.Default)
                 .Subscribe(z =>
                 {
@@ -74,7 +159,7 @@ namespace DofusKeyFinder
                 });
         }
 
-        public static String Checksum(String key)
+        public static string Checksum(string key)
         {
             int checksum = 0;
             for (int i = 0; i < key.Length; i++)
@@ -83,7 +168,8 @@ namespace DofusKeyFinder
             }
             return char.ToString(HEX_CHARS[checksum % 16]);
         }
-        public static String Decrypt(String data, String key)
+
+        public static string Decrypt(string data, string key)
         {
             int shift = int.Parse(Checksum(key), NumberStyles.HexNumber) * 2;
             var decrypted = new StringBuilder(data.Length);
@@ -97,7 +183,7 @@ namespace DofusKeyFinder
             return HttpUtility.UrlDecode(decrypted.ToString());
         }
 
-        public static String HexToString(String data)
+        public static string HexToString(string data)
         {
             var output = new StringBuilder(data.Length / 2);
             for (var i = 0; i < data.Length; i += 2)
@@ -107,12 +193,12 @@ namespace DofusKeyFinder
             return output.ToString();
         }
 
-        public static String PrepareKey(String data)
+        public static string PrepareKey(string data)
         {
             return HttpUtility.UrlDecode(HexToString(data));
         }
 
-        public static String PreEscape(String input)
+        public static string PreEscape(string input)
         {
             var output = new StringBuilder();
             foreach (var c in input)
@@ -129,12 +215,12 @@ namespace DofusKeyFinder
             return output.ToString();
         }
 
-        public static String FormatKeyExport(string key)
+        public static string FormatKeyExport(string key)
         {
             return PreEscape(key).Select(c => String.Format("{0:X}", (int)c)).Aggregate("", (acc, c) => acc + c);
         }
 
-        public static HashSet<int> ComputeMaxShifts(String s)
+        public static HashSet<int> ComputeMaxShifts(string s)
         {
             var shifts = new HashSet<int>();
             int matchPos = 0, maxLength = 0;
@@ -159,13 +245,13 @@ namespace DofusKeyFinder
             return shifts;
         }
 
-        public static String LeftRotateShift(String key, int shift)
+        public static string LeftRotateShift(string key, int shift)
         {
             shift %= key.Length;
             return key.Substring(shift) + key.Substring(0, shift);
         }
 
-        public static String RightRotateShift(String key, int shift)
+        public static string RightRotateShift(string key, int shift)
         {
             shift %= key.Length;
             return key.Substring(key.Length - shift) + key.Substring(0, key.Length - shift);
@@ -176,14 +262,14 @@ namespace DofusKeyFinder
             return (a == 0 || b == 0) ? a | b : GCD(Math.Min(a, b), Math.Max(a, b) % Math.Min(a, b));
         }
 
-        public static int ComputeKeyLengthKasysky(String encodedData)
+        public static int ComputeKeyLengthKasysky(string encodedData)
         {
             return ComputeMaxShifts(encodedData)
                 .Where(shift => shift >= MinKeyLength)
                 .Aggregate(GCD);
         }
 
-        public static int ComputeHammingDistance(String message, int x, int y, int keyLength)
+        public static int ComputeHammingDistance(string message, int x, int y, int keyLength)
         {
             var distance = 0;
             for (var i = 0; i < keyLength; i++)
@@ -205,7 +291,7 @@ namespace DofusKeyFinder
             100% Accurate
 
          */
-        public static int ComputeKeyLengthHamming(String encodedData)
+        public static int ComputeKeyLengthHamming(string encodedData)
         {
             var bestKeyLength = 0;
             var bestScore = int.MaxValue;
@@ -232,54 +318,88 @@ namespace DofusKeyFinder
             return bestKeyLength;
         }
 
-        private static String GuessKey(String message, int keyLength, Frequencies frequency)
+        private static ComputedKey GuessKey(string message, int keyLength, Frequencies frequencies)
         {
-            var numberOfBlock = message.Length / keyLength;
+            var alternatives = new Dictionary<int, List<(int, double)>>();
+            var blockSize = keyLength;
+            var numberOfBlock = message.Length / blockSize;
             var key = new StringBuilder();
-            for (var indexInBlock = 0; indexInBlock < keyLength; indexInBlock++)
+            for (var blockOffset = 0; blockOffset < blockSize; blockOffset++)
             {
+                alternatives.Add(blockOffset, new List<(int, double)>());
                 var bestError = double.MaxValue;
-                var bestXor = 0;
+                var bestXor = -1;
                 // Only between thoses
-                for (var j = MinKeyXor; j <= MaxKeyXor; j++)
+                for (var xorKey = MinKeyXor; xorKey <= MaxKeyXor; xorKey++)
                 {
                     var decryptedBlock = new char[numberOfBlock];
-                    for (var k = 0; k < numberOfBlock; k++)
+                    for (var blockNumber = 0; blockNumber < numberOfBlock; blockNumber++)
                     {
-                        var currentData = message[k * keyLength + indexInBlock];
-                        var currentKey = j;
-                        decryptedBlock[k] = (char)(currentData ^ currentKey);
+                        var currentData = message[blockNumber * blockSize + blockOffset];
+                        decryptedBlock[blockNumber] = (char)(currentData ^ xorKey);
                     }
-                    var decrypted = HttpUtility.UrlDecode(new String(decryptedBlock));
-                    var error = ComputeError(decrypted, indexInBlock, frequency);
+                    var decrypted = HttpUtility.UrlDecode(new string(decryptedBlock));
+                    var error = ComputeError(decrypted, blockOffset, blockSize, frequencies);
                     if (error <= bestError)
                     {
+                        if (error == bestError)
+                        {
+                            if (bestXor != -1)
+                            {
+                                alternatives[blockOffset].Add((xorKey, error));
+                            }
+                        }
+                        else
+                        {
+                            var clampedValue = error * ErrorClamp;
+                            alternatives[blockOffset].RemoveAll(x => x.Item2 > clampedValue);
+                            alternatives[blockOffset].Add((xorKey, error));
+                        }
                         bestError = error;
-                        bestXor = j;
+                        bestXor = xorKey;
                     }
                 }
                 key.Append((char)bestXor);
             }
             // Shift back the key with its checksum
-            var StringKey = key.ToString();
-            int shift = int.Parse(Checksum(StringKey), NumberStyles.HexNumber) * 2;
-            return RightRotateShift(StringKey, shift);
+            var computedKey = key.ToString();
+            int shift = int.Parse(Checksum(computedKey), NumberStyles.HexNumber) * 2;
+            var finalKey = RightRotateShift(computedKey, shift);
+            return new ComputedKey
+            {
+                Value = finalKey,
+                Alternatives = alternatives.Where(x => x.Value.Count > 1).ToDictionary(x => x.Key, x => x.Value)
+            };
         }
 
-        private static double ComputeError(String decrypted, int indexInBlock, Frequencies frequency)
+        private static double ComputeError(string decrypted, int blockOffset, int blockSize, Frequencies frequencies)
         {
             var currentFrequencies = GetFrequencies(decrypted);
-            /*
-                I don't know how to use that position frequency, seems to be 
-                hard, even if the blocks are essentially based on the same
-                characters
-             */
-            var positionFrequencies = frequency.Position[indexInBlock % 10];
-            return GetFrequencyDistance(currentFrequencies, frequency.Global);
+            return GetPositionError(decrypted, blockOffset, blockSize, frequencies);
+        }
+
+        public static double GetPositionError(string decrypted, int blockOffset, int blockSize, Frequencies frequencies)
+        {
+            var distance = 0.0;
+            for (var i = 0; i < decrypted.Length; i++)
+            {
+                var absolutePosition = blockSize * i + blockOffset;
+                var positionFrequencies = frequencies.Position[absolutePosition % CellPatternSize];
+                var currentData = decrypted[i];
+                if (positionFrequencies.ContainsKey(currentData))
+                {
+                    distance += (1 - positionFrequencies[currentData]);
+                }
+                else
+                {
+                    distance += 10;
+                }
+            }
+            return distance;
         }
 
         // Squared Euclidean distance
-        public static double GetFrequencyDistance(Dictionary<char, float> u, Dictionary<char, float> v)
+        public static double GetDistance(Dictionary<char, float> u, Dictionary<char, float> v)
         {
             var distance = 0.0;
             foreach (var kv in u)
@@ -296,7 +416,7 @@ namespace DofusKeyFinder
             return distance;
         }
 
-        private static Dictionary<char, float> GetFrequencies(String input)
+        private static Dictionary<char, float> GetFrequencies(string input)
         {
             var frequencies = new Dictionary<char, int>();
             foreach (var c in input)
